131b05c52265c4518ba6f86d42228b91b643f1fe
Quwam Binuyo <qbinuyo7@gmail.com>
2025-08-28 14:24:54 +0100

    did the same search layout for the mobile too

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,14 +101,16 @@ const Navbar = () => {
           <CustomInput
             value={q}
             onChange={(e) => setQ(e.target.value)}
-            className="w-full pl-10 pr-3 py-2 border border-primary-color rounded-md focus:outline-none"
+            className="w-full pl-19 pr-3 py-2text-primary-color rounded-md font-semibold p-2 
+             border border-transparent focus:border-primary-color focus:ring-0 
+             outline-none"
             placeholder="Search product..."
           />
           <button
-            className="cursor-pointer absolute left-3 top-1/2 transform -translate-y-1/2 text-primary-color text-xl"
+            className="absolute left-0 bg-primary-color cursor-pointer top-1/2 transform -translate-y-1/2 text-white p-2 px-5 h-full  flex justify-center items-center text-xl"
             type="submit"
           >
-            <AiOutlineSearch />
+            <AiOutlineSearch className="text-[25px]" />
           </button>
         </form>
       </div>
