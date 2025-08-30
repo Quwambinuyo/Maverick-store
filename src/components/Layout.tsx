11c419d958420f7c108079bcd5cdaea1ba13f8ea
Quwam Binuyo <qbinuyo7@gmail.com>
2025-08-30 12:27:07 +0100

    added the smooth scroll on each navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,21 @@ import Navbar from "./Navbar";
 import { Outlet, useLocation } from "react-router-dom";
 import { useSidebarStore } from "../features/store";
 import Loader from "./Loader";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuthStore } from "../features/useAuthStore";
 
 const Layout = () => {
   const { isOpen, setLoading, loading } = useSidebarStore();
   const { fetchUser } = useAuthStore();
   const location = useLocation();
+
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [location.pathname]);
 
   useEffect(() => {
     setLoading(true);
@@ -25,6 +33,13 @@ const Layout = () => {
     fetchUser();
   }, []);
 
+  useEffect(() => {
+    console.log(location.pathname);
+
+    // window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="flex h-screen overflow-hidden relative">
       {loading && <Loader />}
@@ -34,7 +49,10 @@ const Layout = () => {
           ${isOpen ? "sm:pl-60" : "sm:pl-25"}`}
       >
         <Navbar />
-        <main className="flex-1 overflow-y-auto  bg-gray-50 pb-19 min-h-screen">
+        <main
+          className="flex-1 overflow-y-auto  bg-gray-50 pb-19 min-h-screen"
+          ref={mainRef}
+        >
           <Outlet />
         </main>
       </div>
